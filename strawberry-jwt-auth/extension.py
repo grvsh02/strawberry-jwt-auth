08c78e03a02ebe56d2eed4c359f7c1663080edc6
Gaurav <gaurav021201@gmail.com>
2022-09-26 00:37:46 +0530

    removed logs

--- strawberry-jwt-auth/extension.py
+++ strawberry-jwt-auth/extension.py
@@ -40,36 +40,28 @@ class JWTExtension(Extension):
             token_payload = self.get_token_payload(tokenName="JWT_REFRESH_TOKEN")
             if token_payload and token_payload is not None:
                 try:
-                    from ..models.refreshTokens import RefreshTokens
+                    from refreshTokens import RefreshTokens
                     refreshTokenObj = RefreshTokens.objects.get(refreshToken=self.request.COOKIES['JWT_REFRESH_TOKEN'])
-                    # refreshTokenObj = RefreshTokens.objects.get(refreshToken=token_payload["refreshToken"])
-                    print("refreshTokenObj", refreshTokenObj)
                     if refreshTokenObj:
                         self.userID = refreshTokenObj.user.id
                         return True
                 except RefreshTokens.DoesNotExist:
-                    print("refreshTokenObj does not exist")
                     pass
-            print("refresh token invalid !!!!!!!!!!!!")
             self.revokeTokens = True
             return False
 
     def on_request_start(self):
         self.request = self.execution_context.context["request"]
-        print("on_request_start", self.request.COOKIES)
         if self.validate_access_token():
             pass
         elif self.validate_refresh_token():
-            print("refresh token validated")
             self.issueNewTokens = True
 
     def resolve(self, _next, root, info, *args, **kwargs):
         setattr(info.context, "userID", self.userID)
         if self.issueNewTokens:
-            print(self.userID)
             setattr(info.context.request, "issueNewTokens", self.issueNewTokens)
             setattr(info.context.request, "clientID", self.userID)
         if self.revokeTokens:
-            print("revokeTokens")
             setattr(info.context.request, "revokeTokens", self.revokeTokens)
         return _next(root, info, **kwargs)
--- strawberry-jwt-auth/views.py
+++ strawberry-jwt-auth/views.py
@@ -5,21 +5,16 @@ from jwt_generator import create_access_token, create_refresh_token
 
 def strawberry_auth_view(get_response):
     def add_cookies_response(request, response):
-        print(response)
         if hasattr(request, "issueNewTokens") and request.issueNewTokens:
-            print("issueNewTokens", request.clientID, response)
             response.set_cookie(
                 key="JWT_ACCESS_TOKEN",
                 value=create_access_token(request.clientID)
             )
-            print(response)
             response.set_cookie(
                 key="JWT_REFRESH_TOKEN",
                 value=create_refresh_token(request.clientID)
             )
         if hasattr(request, "revokeTokens") and request.revokeTokens:
-            print("clientID is None")
-            print(request)
             response.delete_cookie("JWT_ACCESS_TOKEN")
             response.delete_cookie("JWT_REFRESH_TOKEN")
         return response
