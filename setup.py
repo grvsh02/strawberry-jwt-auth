4a61d451e50af608904572b365f6f6d36ed5def1
Gaurav <gaurav021201@gmail.com>
2022-09-26 02:15:38 +0530

    bug fixes and improvements

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+import codecs
 from setuptools import setup, find_packages
 import os
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 VERSION = '0.1.0'
 DESCRIPTION = 'A JWT auth library based on Django and strawberry'
@@ -12,6 +16,8 @@ setup(
     author="grvsh02 (Gaurav Sharma)",
     author_email="<gaurav021201@gmail.com>",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['pyjwt', 'strawberry-graphql', 'django', 'phonenumbers'],
     keywords=['python', 'jwt', 'auth', 'strawberry', 'graphql', 'Authentication'],
